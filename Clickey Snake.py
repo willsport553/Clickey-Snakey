5fd8678855695deebd7d96d024257eba65346a25
williamashman <willsport553@gmail.com>
2022-06-17 18:30:24 +1200

    Make the Snake longer - this took too long...

--- Clickey Snake.py
+++ Clickey Snake.py
@@ -33,9 +33,24 @@ snake_size = 30
 fps = 60
 
 clock = pygame.time.Clock()
+font = pygame.font.SysFont(None, 55)
+
+def text_screen(text, color, x, y):
+    screen_text = font.render(text, True, color)
+    gameWindow.blit(screen_text, [x,y])
+
+
+def plot_snake(gameWindow, color, snk_list, snake_size):
+    for x,y in snk_list:
+        pygame.draw.rect(gameWindow, color, [x, y, snake_size, snake_size])
+
+
+snk_list = []
+snk_length = 1
 
 # Game Loop
 while not exit_game:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit_game = True
@@ -62,13 +77,25 @@ while not exit_game:
 
     if abs(snake_x - food_x)<6 and abs(snake_y - food_y)<6:
         score +=1
-        print("Score: ", score * 10)
         food_x = random.randint(20, screen_width / 2)
         food_y = random.randint(20, screen_height / 2)
+        snk_length +=5
 
     gameWindow.fill(white)
+    text_screen("Score: " + str(score * 10), red, 5, 5)
     pygame.draw.rect(gameWindow, red, [food_x, food_y, snake_size, snake_size])
-    pygame.draw.rect(gameWindow, black, [snake_x, snake_y, snake_size, snake_size])
+
+
+    head = []
+    head.append(snake_x)
+    head.append(snake_y)
+    snk_list.append(head)
+
+    if len(snk_list)>snk_length:
+        del snk_list[0]
+
+    # pygame.draw.rect(gameWindow, black, [snake_x, snake_y, snake_size, snake_size])
+    plot_snake(gameWindow, black, snk_list, snake_size)
     pygame.display.update()
     clock.tick(fps)
 
