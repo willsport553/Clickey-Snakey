5df236cdbc50623c3ea9b9d107f00083b72eb233
williamashman <willsport553@gmail.com>
2022-06-17 17:17:03 +1200

    Snake Movement With one key - but set up

--- Clickey Snake.py
+++ Clickey Snake.py
@@ -20,17 +20,25 @@ exit_game = False
 game_over = False
 snake_x = 45
 snake_y = 55
-snake_size = 10 
+snake_size = 10
+fps = 30
 
+clock = pygame.time.Clock()
 # Game Loop
 while not exit_game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit_game = True
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                snake_x = snake_x + 10
+
     gameWindow.fill(white)
     pygame.draw.rect(gameWindow, black, [snake_x, snake_y, snake_size, snake_size])
     pygame.display.update()
+    clock.tick(fps)
+
 
 pygame.quit()
 quit()
