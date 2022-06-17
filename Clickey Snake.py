a2c31137ea27d70bba001f566caa012affe168cd
williamashman <willsport553@gmail.com>
2022-06-17 18:25:56 +1200

    Sscore - random fruit spawn - size diference

--- Clickey Snake.py
+++ Clickey Snake.py
@@ -14,7 +14,7 @@ screen_height = 600
 gameWindow = pygame.display.set_mode((screen_width, screen_height))
 
 # Game Title
-pygame.display.set_caption("Clickey Snake")
+pygame.display.set_caption("SnakesWithHarry")
 pygame.display.update()
 
 # Game specific variables
@@ -25,13 +25,15 @@ snake_y = 55
 velocity_x = 0
 velocity_y = 0
 
-food_x = random.randint(0, screen_width)
-food_y = random.randint(0, screen_height)
-
-snake_size = 10
-fps = 30
+food_x = random.randint(20, screen_width/2)
+food_y = random.randint(20, screen_height/2)
+score = 0
+init_velocity = 5
+snake_size = 30
+fps = 60
 
 clock = pygame.time.Clock()
+
 # Game Loop
 while not exit_game:
     for event in pygame.event.get():
@@ -40,30 +42,35 @@ while not exit_game:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                velocity_x = 10
+                velocity_x = init_velocity
                 velocity_y = 0
 
             if event.key == pygame.K_LEFT:
-                velocity_x = - 10
+                velocity_x = - init_velocity
                 velocity_y = 0
 
             if event.key == pygame.K_UP:
-                velocity_y = - 10
+                velocity_y = - init_velocity
                 velocity_x = 0
 
             if event.key == pygame.K_DOWN:
-                velocity_y = 10
+                velocity_y = init_velocity
                 velocity_x = 0
 
     snake_x = snake_x + velocity_x
     snake_y = snake_y + velocity_y
+
+    if abs(snake_x - food_x)<6 and abs(snake_y - food_y)<6:
+        score +=1
+        print("Score: ", score * 10)
+        food_x = random.randint(20, screen_width / 2)
+        food_y = random.randint(20, screen_height / 2)
 
     gameWindow.fill(white)
     pygame.draw.rect(gameWindow, red, [food_x, food_y, snake_size, snake_size])
     pygame.draw.rect(gameWindow, black, [snake_x, snake_y, snake_size, snake_size])
     pygame.display.update()
     clock.tick(fps)
-
 
 pygame.quit()
 quit()
